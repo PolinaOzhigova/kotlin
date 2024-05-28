8564863481ac69a65429583b2edc7010bbca09e4
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2024-05-28 16:57:25 +0800

    gps

---
+++ GPSLocation24/app/src/main/java/com/example/gpslocation24/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.gpslocation24
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity(), LocationListener {
+    val LOCATION_PERM_CODE = 2
+    lateinit var locationManager: LocationManager
+    lateinit var updateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        updateButton = findViewById(R.id.updButton)
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERM_CODE)
+        } else{
+            LocationUpdate()
+        }
+
+        updateButton.setOnClickListener {
+            displayCoord(0.toDouble(), 0.toDouble())
+            LocationUpdate()
+            Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    fun LocationUpdate(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+
+        val prv = locationManager.getBestProvider(Criteria(), true)
+        Log.d("my", locationManager.allProviders.toString())
+        if (prv != null) {
+            val location = locationManager.getLastKnownLocation(prv)
+            if (location != null)
+                displayCoord(location.latitude, location.longitude)
+            Log.d("mytag", "location set")
+        }
+    }
+
+    override fun onLocationChanged(loc: Location) {
+        val lat = loc.latitude
+        val lng = loc.longitude
+        displayCoord(lat, lng)
+        Log.d("my", "lat: $lat long: $lng")
+    }
+
+    fun displayCoord(latitude: Double, longtitude: Double) {
+        findViewById<TextView>(R.id.lat).text = String.format("%.5f", latitude)
+        findViewById<TextView>(R.id.lng).text = String.format("%.5f", longtitude)
+    }
+
+    override fun onProviderDisabled(provider: String) {
+        Toast.makeText(this, "Provider $provider disabled", Toast.LENGTH_SHORT).show()
+        super.onProviderDisabled(provider)
+    }
+
+    override fun onProviderEnabled(provider: String) {
+        Toast.makeText(this, "Provider $provider enabled", Toast.LENGTH_SHORT).show()
+        super.onProviderEnabled(provider)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERM_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                LocationUpdate()
+            } else{
+                Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERM_CODE)
+            }
+        }
+    }
+}
